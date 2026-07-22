5fde2942d62133b24097aa25c80e4fced8c19946
manh-ai <sonofthegod07@gmail.com>
2026-07-22 18:13:52 +0700

    Add files via upload

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,24 @@
+// toán tử số học = trả về kết quả của một phép tính số học cụ thể (+ - * /)
+// arithmetic operators = return the result of a specific arithmetic operation (+ - * /)
+#include<iostream>
+int main(){
+    //int dollars = 20;
+    //dollars = dollars + 1;
+    //dollars += 1;
+    //dollars ++;
+
+    //dollars = dollars - 1;
+    //dollars -= 6;
+    //dollars --;
+
+    //dollars = dollars * 2;
+    //dollars *= 68;
+    
+    //dollars = dollars / 2;
+    //dollars /= 2;
+
+    //int remainder = dollars % 3;
+    int dollars = 3 + 5 * 2 / 7;
+    std::cout<<dollars;    
+    return 0;
+}
