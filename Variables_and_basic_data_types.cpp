116c28bd87e513a6b04658a5423fa6ad69a389aa
manh-ai <sonofthegod07@gmail.com>
2026-07-22 10:12:22 +0700

    Add files via upload

---
+++ Variables_and_basic_data_types.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+int main(){
+//int(integer number only)
+   int x = 7.9;
+   int y = 8.9;
+   int sum = x + y;
+//double(number using decimal)
+   double m = 6.7;
+   double n = 7.28;
+   double summ = m + n;
+//char(single charater)
+   char h = 'h';
+   char i = 'i';
+   char e = 'e';
+   char p = 'p';
+//bool(two states true and false)
+   bool manh_thich_ca = false;
+   bool manh_thich_pia = true;
+//string(several characters)
+std::string manh = "deptrai";
+std::cout<< manh << "\n";
+std::cout<< manh_thich_pia;
+    return 0;
+}
