8adf861cb1c506852976c85998d32d5c2c228772
manh-ai <sonofthegod07@gmail.com>
2026-07-23 09:46:33 +0700

    Add files via upload

---
+++ math_function.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+int main(){
+    double x = 3;
+    double y = 8;
+    double z;
+// z = std::max(x,y);
+// z = std::min(x,y);
+// z = pow(3,8);
+// z = sqrt(6561);
+// z = round(3.14);
+// z = ceil (2.1);
+// z = floor (3.9);
+// z = abs(-3);
+    std::cout<<z;
+    return 0;
+}
