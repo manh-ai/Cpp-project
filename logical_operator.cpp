dd82eec6e4c18b1d6ae2a15d4ce796f5653eca37
manh-ai <sonofthegod07@gmail.com>
2026-07-24 09:18:59 +0700

    Add files via upload

---
+++ logical_operator.cpp
@@ -0,0 +1,29 @@
+// && = kiểm tra xem liệu cả hai trường hợp có đúng
+// || = kiểm tra xem liệu có một trong hai trường hợp đúng
+// !  = đảo ngược trạng thái logic của toán hạng
+
+// && = check if two conditions are true
+// || = check if at least one of two conditions is true
+// !  = reverses the logic state of its operand
+
+#include<iostream>
+int main(){
+//int temp = 14;
+//if(temp > 10 && temp <30){
+//   std::cout<<"the weather is good!";
+//}
+//if(temp < 10 || temp > 30){
+//    std::cout<<"the weather is bad!";
+//}
+//else{
+//   std::cout<<"the weather is good!";
+//}
+bool sunny = false;
+if(sunny){
+    std::cout<<"it's sunny outside";
+}
+else{
+    std::cout<<"it's coudy outside";
+}
+    return 0;
+}
---
+++ ternary_operator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main(){
+// tenary operater ?: = replacement to if/else statement
+// condition ? expression1 : expression2
+
+//int grade = 36;
+//grade >= 50 ? std::cout<<"You pass!" : std::cout<<"You fail!";
+
+//bool hungry = false;
+//hungry ? std::cout<<"You are hungry" : std::cout<<"You are full";
+
+int a = 18;
+a % 2 == 0 ? std::cout<<"even" : std::cout<<"odd";
+
+
+    return 0;
+}
